1deb9e2d02b414a992b201acdfb22108f1017ce5
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-11-11 12:02:01 +0200

    refactor: remove unused components and clean up imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-"use client";
-import Accommodation from "./Accommodation/Accommodation";
-import Booklet from "./Booklet/Booklet";
-import Dress from "./Dress/Dress";
-import Footer from "./Footer/Footer";
-import Header from "./Header/Header";
-import Schedule from "./Schedule/Schedule";
-import Shuttle from "./Shuttle/Shuttle";
-import Synopsis from "./Synopsis/Synopsis";
-import { ParallaxProvider } from "react-scroll-parallax";
+'use client';
+import Booklet from './Booklet/Booklet';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Synopsis from './Synopsis/Synopsis';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 export default function Home() {
   return (
@@ -16,10 +12,6 @@ export default function Home() {
         <Header />
       </ParallaxProvider>
       <Synopsis />
-      <Schedule />
-      <Accommodation />
-      <Shuttle />
-      <Dress />
       <Booklet />
       <Footer />
     </div>
